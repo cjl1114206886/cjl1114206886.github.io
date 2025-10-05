8764fcf1214508d85067c1f5508634030d7a5149
cjl1114206886 <1114206886@gmail.com>
2025-10-06 01:39:13 +0800

    add production deploy workflow

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "cjl1114206886.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
